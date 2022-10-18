187ad17297c307b1bdf1f48bf2b0803f1a90e87b
Lorenzo Bodini <32436755+topongo@users.noreply.github.com>
2022-10-18 12:17:30 +0200

    add auto-answerer

--- rischioBasso.js
+++ rischioBasso.js
@@ -13,41 +13,67 @@
 //
 
 
-if (Notification.permission !== "denied")Notification.requestPermission();
+if (Notification.permission !== "denied")Notification.requestPermission()
 
 var iframedoc = document.getElementById("scorm_object").contentWindow.document
-var observer = new MutationObserver(function(mutations) {
-  console.log("Mutation detected.");
-  if(mutations[0].attributeName == "title"){
-    if(mutations[0].target.getAttribute("title") == "Play"){
-      console.log("Video Paused");
-      console.log("Trying to find a \"continue\" button...");
+
+
+// click the button on screen if Click_Box
+function clickIfClickBox(item){
+  if(item.id.includes("Click_Box")){
+    console.log("Found one Click_Box suitable for clicking:")
+    console.log(item)
+    item.click()
+    return true
+  }
+  return false
+}
+
+function answerAndSend(){
+  for(let item of iframedoc.getElementsByTagName("input")){
+    if(item.type == "radio"){
+      item.click()
       for(let item of iframedoc.getElementsByClassName("cp-frameset")){
-        if(item.id.includes("Click_Box")){
-          console.log("Found one Click_Box suitable for clicking:");
-          console.log(item);
-          item.click();
-        }
-        if(item.getAttribute("aria-label") && item.getAttribute("aria-label").includes("INIZIA IL TEST")){
-          console.log("Detected exercise")
-          var notification = new Notification("Corso sicurezza: interazione richiesta!");
+        //                                                                                        ↓↓↓        I am assuming this       ↓↓↓
+        if(item.hasAttribute("aria-label") && (item.getAttribute("aria-label") == "Conferma " || item.getAttribute("aria-label") == "Confirm ")){
+           item.click()
+           function wait(){
+             let banner = iframedoc.getElementById("feedbackClickDiv")
+             if(banner)
+               banner.click()
+             else
+               setTimeout(wait, 100)
+           }
+           setTimeout(wait, 100)
         }
       }
     }
-    else if(mutations[0].target.getAttribute("title") == "Pause")console.log("Video Played");
   }
-});
+}
+
+var observer = new MutationObserver(function(mutations) {
+  console.log("Mutation detected.")
+  if(mutations[0].attributeName == "title"){
+    if(mutations[0].target.getAttribute("title") == "Play"){
+      console.log("Video Paused")
+      console.log("Trying to find a \"continue\" button...")
+      for(let item of iframedoc.getElementsByClassName("cp-frameset")){
+        if(clickIfClickBox(item)) break
+        if(item.getAttribute("aria-label") && item.getAttribute("aria-label").includes("INIZIA IL TEST")){
+          console.log("Detected exercise")
+          var notification = new Notification("Corso sicurezza: interazione richiesta!")
+        }
+      }
+      answerAndSend()
+    }
+    else if(mutations[0].target.getAttribute("title") == "Pause")console.log("Video Played")
+  }
+})
 
 observer.observe(iframedoc.getElementsByClassName("playbarBigButton")[0], {
     attributes: true
-});
+})
 
-
-// click the button on screen if there is any
 for(let item of iframedoc.getElementsByClassName("cp-frameset")){
-  if(item.id.includes("Click_Box")){
-    console.log("Found one Click_Box suitable for clicking:");
-    console.log(item);
-    item.click();
-  }
+  clickIfClickBox(item)
 }
